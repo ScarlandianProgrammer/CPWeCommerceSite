d3bf028d8c8c5579e7b61232ad6158c5f863f5f4
Elias McKee <eqbookfan@gmail.com>
2023-07-22 07:52:02 -0700

    Initial Commit.

---
+++ CPWeCommerceSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CPWeCommerceSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
