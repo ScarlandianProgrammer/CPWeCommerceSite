4fead61ad9495f98b436ef30899733b4d26eae43
Elias McKee <eqbookfan@gmail.com>
2023-08-10 17:34:16 -0700

    Added properties to the Product class

--- CPWeCommerceSite/Models/Product.cs
+++ CPWeCommerceSite/Models/Product.cs
@@ -1,10 +1,28 @@
-﻿namespace CPWeCommerceSite.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPWeCommerceSite.Models
 {
     /// <summary>
     /// Represents a product that this company sells.
     /// </summary>
     public class Product
     {
+        /// <summary>
+        /// The unique identifier for each product
+        /// </summary>
+        [Key]
+        public int ProductId { get; set; }
 
+        /// <summary>
+        /// The name of a product that the company offers
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The price of a product that the company offers
+        /// </summary>
+        public double Price { get; set; }
+        // can be null?
     }
 }
