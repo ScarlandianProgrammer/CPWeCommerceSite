a7fb1dc5d8e5ccb78d91251168d6759493b41e56
Elias McKee <eqbookfan@gmail.com>
2023-08-11 15:36:02 -0700

    Added DBContext class

---
+++ CPWeCommerceSite/Data/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using CPWeCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPWeCommerceSite.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
