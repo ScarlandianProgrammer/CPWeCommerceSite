3abb5a498bfc46f1045df451bc549d66dc08e884
Elias McKee <eqbookfan@gmail.com>
2023-08-19 11:52:54 -0700

    Created the Create.cs view

---
+++ CPWeCommerceSite/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPWeCommerceSite.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
