97ce14fa1f4ed2a5c45ff7f0828c3fe44ebf1965
Elias McKee <eqbookfan@gmail.com>
2023-08-19 13:10:29 -0700

    Added functionality to the Create page

--- CPWeCommerceSite/Controllers/ProductsController.cs
+++ CPWeCommerceSite/Controllers/ProductsController.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPWeCommerceSite.Data;
+using CPWeCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPWeCommerceSite.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product) 
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to DB
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                // show success message on page
+                ViewData["Message"] = $"{product.Title} was added successfully!";
+                return View();
+            }
+            return View(product);
         }
     }
 }
