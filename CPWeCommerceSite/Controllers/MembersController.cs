6cc267b7628e73f3bdf64706472a903ffa3be4e3
Elias McKee <eqbookfan@gmail.com>
2023-08-24 16:03:21 -0700

    Added the Member and Registration classes

---
+++ CPWeCommerceSite/Controllers/MembersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPWeCommerceSite.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
--- CPWeCommerceSite/Models/Member.cs
+++ CPWeCommerceSite/Models/Member.cs
@@ -7,12 +7,38 @@ namespace CPWeCommerceSite.Models
         [Key]
         public int MemberId { get; set; }
 
-        public string EmailAddress { get; set; }
+        [Required]
+        public string Email { get; set; }
 
+        [Required]
         public string Password { get; set; }
 
+        [Required]
         public string PhoneNumber { get; set; }
 
+        [Required]
         public string Username { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set;}
     }
 }
