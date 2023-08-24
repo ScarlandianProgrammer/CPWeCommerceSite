1526b2bf168f6d74b675dad41d38e32fd613e827
Elias McKee <eqbookfan@gmail.com>
2023-08-24 15:26:29 -0700

    Added Member class

---
+++ CPWeCommerceSite/Models/Member.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPWeCommerceSite.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberId { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Username { get; set; }
+    }
+}
