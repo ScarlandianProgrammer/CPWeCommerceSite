c23ff8a974bf4ac64507c597e26cd49f77366827
Elias McKee <eqbookfan@gmail.com>
2023-08-24 15:29:39 -0700

    Added migration for Member class

--- CPWeCommerceSite/Migrations/20230824222734_AddedMember.Designer.cs
+++ CPWeCommerceSite/Migrations/20230824222734_AddedMember.Designer.cs
@@ -4,6 +4,7 @@ using CPWeCommerceSite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CPWeCommerceSite.Migrations
 {
     [DbContext(typeof(ProductContext))]
-    partial class ProductContextModelSnapshot : ModelSnapshot
+    [Migration("20230824222734_AddedMember")]
+    partial class AddedMember
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
