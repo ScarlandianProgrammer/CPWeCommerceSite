b92ba99e815116d9045de9d48daa500e92481287
Elias McKee <eqbookfan@gmail.com>
2023-08-25 14:21:02 -0700

    Added Register view

--- CPWeCommerceSite/Models/Member.cs
+++ CPWeCommerceSite/Models/Member.cs
@@ -34,11 +34,13 @@ namespace CPWeCommerceSite.Models
 
         [Required]
         [StringLength(75, MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
         [Compare(nameof(Password))]
         [Display(Name = "Confirm Password")]
-        public string ConfirmPassword { get; set;}
+		[DataType(DataType.Password)]
+		public string ConfirmPassword { get; set;}
     }
 }
