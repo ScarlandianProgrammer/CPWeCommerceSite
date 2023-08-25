caca74f6e19d2f134a79087daca5813dbb17cb8e
Elias McKee <eqbookfan@gmail.com>
2023-08-25 15:01:51 -0700

    Implimented the Register view

--- CPWeCommerceSite/Controllers/MembersController.cs
+++ CPWeCommerceSite/Controllers/MembersController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPWeCommerceSite.Data;
+using CPWeCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPWeCommerceSite.Controllers
 {
     public class MembersController : Controller
     {
+		private readonly ProductContext _context;
+
+		public MembersController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel newMember)
+        {
+            if (ModelState.IsValid)
+            {
+                // map RegisterViewModel to Member
+                Member member = new()
+                {
+                    Email = newMember.Email,
+                    Password = newMember.Password
+                };
+				_context.Members.Add(member);
+				await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+			}
+            return View(newMember);
         }
     }
 }
--- CPWeCommerceSite/Models/Member.cs
+++ CPWeCommerceSite/Models/Member.cs
@@ -8,16 +8,14 @@ namespace CPWeCommerceSite.Models
         public int MemberId { get; set; }
 
         [Required]
-        public string Email { get; set; }
+        public string Email { get; set; } = null!;
 
         [Required]
-        public string Password { get; set; }
+        public string Password { get; set; } = null!;
 
-        [Required]
-        public string PhoneNumber { get; set; }
+		public string? PhoneNumber { get; set; }
 
-        [Required]
-        public string Username { get; set; }
+        public string? Username { get; set; }
     }
 
     public class RegisterViewModel
